d328b3c9ee0050efa1c5a2e9960ac74c78ca8076
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-03-12 20:39:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/ipv6-helper/',
+export default defineConfig(({ mode } ) => ({
+  base: '/', // ALTERADO: Definido para a raiz do domínio
   server: {
     host: "::",
     port: 8080,
